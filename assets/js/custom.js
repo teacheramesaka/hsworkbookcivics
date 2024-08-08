99451fa12b1f38f77a4992a7595f980f07924649
amesaka <teacheramesaka@gmail.com>
2024-08-08 16:44:46 +0900

    サイドバーの折り畳み機能追加

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var toggles = document.querySelectorAll('.book-summary .chapter > .toggle');
+    var chapters = document.querySelectorAll('.book-summary .chapter');
     
+    // 初期状態ですべての章を折りたたむ
+    chapters.forEach(function(chapter) {
+        chapter.classList.remove('active');
+    });
+
     toggles.forEach(function(toggle) {
         toggle.addEventListener('click', function() {
             var chapter = toggle.parentElement;
